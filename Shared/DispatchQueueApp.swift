54b7fb7b205187add0d615bbeacb3be76600ce7d
borna libertines <2532576+boriskol@users.noreply.github.com>
2022-02-09 22:46:44 -0600

    Initial Commit

---
+++ Shared/DispatchQueueApp.swift
@@ -0,0 +1,17 @@
+//
+//  DispatchQueueApp.swift
+//  Shared
+//
+//  Created by Borna Libertines on 09/02/22.
+//
+
+import SwiftUI
+
+@main
+struct DispatchQueueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
